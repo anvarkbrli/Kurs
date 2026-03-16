5c57f12488156cc7e7f5508d5d288cb62eab83b8
YusifBashirzada <yusifsb-apa103@code.edu.az>
2026-03-16 14:03:09 +0400

    fisrt

---
+++ Models/Teacher.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Models
+{
+    public class Teacher
+    {
+        string _username;
+        string _firstname;
+        string _lastname;
+        string _departmentName;
+        string _subject;
+        string _birthdate;
+        string _address;
+        int _phoneNumber;
+        string _email;
+        string _password;
+
+        public Teacher()
+        {
+
+        }
+
+        public Teacher(string firstname) : this()
+        {
+            _firstname = firstname;
+        }
+
+        public Teacher(string firstname, string lastname) : this(firstname)
+        {
+            _lastname = lastname;
+        }
+
+        public Teacher(string firstname, string lastname, string email) : this(firstname, lastname)
+        {
+            _email = email;
+        }
+
+        public Teacher(string firstname, string lastname, string email, string password) : this(firstname, lastname, email)
+        {
+            _password = password;
+        }
+
+        public Teacher(string firstname, string lastname, string email, string password, string username) : this(firstname, lastname, email, password)
+        {
+            _username = username;
+        }
+
+        public Teacher(string firstname, string lastname, string email, string password, string username, int phoneNumber) : this(firstname, lastname, email, password, username)
+        {
+            _phoneNumber = phoneNumber;
+        }
+
+        public Teacher(string firstname, string lastname, string email, string password, string username, string birthdate, string subject) : this(firstname, lastname, email, password, username)
+        {
+            _birthdate = birthdate;
+            _subject = subject;
+        }
+
+
+        public string GetName()
+        {
+            return $"{_firstname} {_lastname}";
+        }
+
+        public string GetSubject()
+        {
+            return $"{_subject}";
+        }
+
+        public string GetTeacher()
+        {
+            return $"{_firstname} {_lastname} {_email} {_username}";
+        }
+
+        public string GetEmail()
+        {
+            return $"{_email}";
+        }
+
+        public string GetBirthdate()
+        {
+            return $"{new DateOnly(2006, 1, 6)}";
+        }
+
+        public string GetContact()
+        {
+            return $"{_phoneNumber} {_email}";
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,12 @@
-﻿namespace ConsoleApp1
+﻿using ConsoleApp1.Models;
+
+namespace ConsoleApp1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Teacher teacher = new Teacher("Yusif", "Besirzade", "yy999@gmail.com", "yusif123!", "Yusif123", 0559434323, new DateOnly(2006, 1, 6), "Math");
         }
     }
 }
